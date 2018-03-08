99eefd8a0146e583eb033958c57bb2b646bbeba3
Inflation <hypernovagama@gmail.com>
2018-03-08 14:59:30 -0500

    Complete basic functionality to download a page of images

--- app/Main.hs
+++ app/Main.hs
@@ -6,35 +6,35 @@ import Lib
 
 import Control.Monad.IO.Class
 import Data.Monoid
-import System.IO
+import System.IO hiding (hPutStr)
 import System.Directory
 import qualified Data.Text as T
 import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy as BS
+import Data.ByteString.Lazy (hPutStr)
 import Data.Sequence
 
 import Data.Default
 import Network.HTTP.Req
 
 main :: IO ()
-main =
-  runReq def $ do
-    r <-
-      getPosts
-        (https "konachan.com" /: "post.json")
-        (mkParams [("tags", "rating:explicit")])
-    rawData <- getImage (fileUrl $ head r)
-    liftIO $ createFolder "image"
-    liftIO $ saveImage (getImagePath "image" $ head r) rawData
+main = createFolder "image" >> serial >>= mapM_ saveImage
 
 createFolder :: FilePath -> IO ()
 createFolder = createDirectoryIfMissing True
 
-saveImage :: FilePath -> BS.ByteString -> IO ()
-saveImage filepath rawData = do
+saveImage :: DownloadResult -> IO ()
+saveImage (filepath, rawData) = do
   withBinaryFile filepath WriteMode $ \h -> do
     hSetBuffering h (BlockBuffering Nothing)
-    BS.hPutStr h rawData
+    hPutStr h rawData
 
--- singleReq :: Req (a) -> IO ((B.ByteString, BS.ByteString))
--- singleReq r = 
+singleReq :: Req (Post) -> IO ((FilePath, BinaryContent))
+singleReq r = runReq def $ r >>= downloadPost "image"
+
+serial :: IO ([DownloadResult])
+serial =
+  runReq def $
+  getPosts
+    (https "konachan.com" /: "post.json")
+    (mkParams [("tags", "rating:safe")]) >>=
+  downloadPosts "image"
--- src/Lib.hs
+++ src/Lib.hs
@@ -15,14 +15,24 @@ import Data.Aeson.Types
 import Network.HTTP.Req
 import System.FilePath
 
+type BinaryContent = BS.ByteString
+
 data Post = Post
   { md5 :: String
   , fileUrl :: String
+  , previewUrl :: String
   } deriving (Show)
 type Posts = [Post]
 
 instance FromJSON Post where
-  parseJSON = withObject "post" $ \o -> Post <$> o .: "md5" <*> o .: "file_url"
+  parseJSON =
+    withObject "post" $ \o ->
+      Post <$> o .: "md5" <*> o .: "file_url" <*> o .: "preview_url"
+
+getUrl :: Post -> String
+getUrl p = previewUrl p
+
+type DownloadResult = (FilePath, BinaryContent)
 
 mkParams :: (QueryParam param, Monoid param) => [(String, String)] -> param
 mkParams params = foldr step (queryFlag "a") params
@@ -36,7 +46,7 @@ getPosts url option = do
     Left e -> fail e
     Right result -> return result
 
-getImage :: String -> Req (BS.ByteString)
+getImage :: String -> Req (BinaryContent)
 getImage imageUrl =
   case (parseUrlHttps $ B.pack imageUrl) of
     Nothing -> fail "Parse URL Error"
@@ -46,4 +56,13 @@ getImage imageUrl =
 
 getImagePath :: FilePath -> Post -> FilePath
 getImagePath base post =
-  base </> (md5 post) ++ '.' : (last $ splitOn "." (fileUrl post))
+  base </> (md5 post) ++ '.' : (last $ splitOn "." (getUrl post))
+
+downloadPost :: FilePath -> Post -> Req (DownloadResult)
+downloadPost base p = do
+  let path = getImagePath base p
+  rawData <- getImage (getUrl p)
+  return (path, rawData)
+
+downloadPosts :: FilePath -> Posts -> Req [(DownloadResult)]
+downloadPosts base ps = mapM (downloadPost base) ps
