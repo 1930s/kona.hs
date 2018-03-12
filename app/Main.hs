18a6fac45a84b0a98eb16c4bf9f9264b1b4cc0d6
Inflation <hypernovagama@gmail.com>
2018-03-12 17:51:32 -0400

    First concurent try

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@ import Lib
 import Utils
 
 import Control.Monad.IO.Class
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TMQueue
 import Data.Monoid
 import System.IO hiding (hPutStr)
 import System.Directory
@@ -18,7 +21,7 @@ import Data.Default
 import Network.HTTP.Req
 
 main :: IO ()
-main = createFolder "image" >> serial 
+main = createFolder "image" >> parallel
 
 createFolder :: FilePath -> IO ()
 createFolder = createDirectoryIfMissing True
@@ -30,8 +33,17 @@ saveImage (filepath, rawData) = do
     hPutStr h rawData
 
 getPosts :: Option Https -> IO [Post]
-getPosts option = runReq def $
-  reqPosts (https "konachan.com" /: "post.json") option
+getPosts option =
+  runReq def $ reqPosts (https "konachan.com" /: "post.json") option
+
+getPostsChan :: Option Https -> IO (TMQueue Post)
+getPostsChan option = do
+  ps <- getPosts option
+  atomically $ do
+    tq <- newTMQueue
+    mapM_ (writeTMQueue tq) ps
+    closeTMQueue tq
+    return tq
 
 downloadPost :: Post -> IO ()
 downloadPost p = (runReq def $ reqPost "image" p) >>= saveImage
@@ -39,3 +51,17 @@ downloadPost p = (runReq def $ reqPost "image" p) >>= saveImage
 serial :: IO ()
 serial =
   getPosts (mkParams [tags ["touhou", rating "safe"]]) >>= mapM_ downloadPost
+
+parallel :: IO ()
+parallel =
+  getPostsChan (mkParams [tags ["touhou", rating "safe"]]) >>= loop >> return ()
+  where
+    loop tq = do
+      p <- atomically $ readTMQueue tq
+      case p of
+        Just post -> do
+          forkFinally (downloadPost post) handleErr
+          loop tq
+        Nothing -> return ()
+    handleErr (Left e) = print e
+    handleErr (Right _) = return ()
